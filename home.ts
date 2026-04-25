e8e6b6ab6c3c785bbbab44864c415394fbb0a206
Abdul Shihab Ansari <shihabbot123@gmail.com>
2026-04-25 22:06:19 +0530

    IG Logo Local

---
+++ home.ts
@@ -0,0 +1,17 @@
+import { HTMLElement } from 'domits';
+import * as css from './styles.css';
+
+const HOME = {
+  logo: {
+    type: 'image',
+    src: 'images/Instagram Logo.jpg',
+    alt: 'Instagram Logo'
+  },
+  button: {
+    class: 'instagram-button',
+    type: 'button',
+    text: 'Connect with Instagram'
+  }
+};
+
+export default HOME;
